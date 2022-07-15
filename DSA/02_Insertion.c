257837f1d8f83e7dd651018c7e073e62f51774d2
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-07-15 21:16:26 +0530

    Insertion of an element in an array

---
+++ DSA/02_Insertion.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void showVal(int arr[], int n) {
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+int indInsertion(int arr[], int size, int element, int capacity, int index) {
+ if (size >= capacity) {
+  return -1;
+ }
+
+ for (int i = size-1; i >= index; i--) {
+  arr[i+1] = arr[i];
+ }
+
+ arr[index] = element;
+ return 1;
+}
+
+int main() {
+  int arr[100] = {7, 8, 12, 27, 88};
+  int size = 5, element = 45, index = 3;
+
+  showVal(arr, size);
+
+  indInsertion(arr, size, element, 100, index);
+  size += 1;
+  
+  showVal(arr, size);
+
+  return 0;
+}
+
