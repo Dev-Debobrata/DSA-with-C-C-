6b041d78a647c1efb7b93eab905554f4edf00d44
Dev-Destructor <dev.debobratapal@gmail.com>
2023-07-02 01:02:52 +0530

    added quick sort

--- DSA_With_C/Algorithms/Sorting/Quick_Sort.c
+++ DSA_With_C/Algorithms/Sorting/Quick_Sort.c
@@ -7,16 +7,39 @@ void swap(int *val1, int *val2)
   *val2 = temp;
 }
 
-void quick_sort(int arr[], int size)
+int partition(int arr[], int low, int high)
 {
+  int pivot = arr[high];
+  int i = (low - 1);
+  for (int j = low; j < high; j++)
+  {
+    if (arr[j] <= pivot)
+    {
+      i++;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
+}
+
+void quick_sort(int arr[], int low, int high)
+{
+  if (low < high)
+  {
+    int pi = partition(arr, low, high);
+    quick_sort(arr, low, pi - 1);
+    quick_sort(arr, pi + 1, high);
+  }
 }
 
 int main()
 {
   int arr[] = {5, 2, 4, 6, 1, 3};
   int size = sizeof(arr) / sizeof(arr[0]);
+  int low = 0, high = size - 1;
 
-  quick_sort(arr, size);
+  quick_sort(arr, low, high);
 
   for (int i = 0; i < size; i++)
   {
